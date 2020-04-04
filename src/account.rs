669ec9789bc89a7ee43adfd92d752ed92cadb227
jmcph4 <jmcph4.github@gmail.com>
2020-04-04 17:02:47 +1000

    Add equality for accounts
    This commit adds equality for accounts.

--- src/account.rs
+++ src/account.rs
@@ -1,12 +1,13 @@
 use std::collections::HashMap;
 
 pub type AccountId = u128;
+pub type AccountBalance = u128;
 
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Debug, PartialEq, Eq, Default)]
 pub struct Account {
     id: AccountId,
     name: String,
-    balance: f64,
+    balance: AccountBalance,
     holdings: HashMap<String, u64>
 }
 
