f48fe1e6949a67a1ae1479dec8a6d61e699855c1
jmcph4 <jmcph4.github@gmail.com>
2020-04-06 23:21:38 +1000

    Define constructor for `Book` type
    This commit defines the constructor for the `Book` type.

--- src/book.rs
+++ src/book.rs
@@ -14,9 +14,22 @@ pub type BookId = u128;
 pub struct Book<'a> {
     pub id: BookId,
     pub name: String,
-    pub ticker: &'a str,
+    pub ticker: String,
     pub bids: Side<'a>,
     pub asks: Side<'a>,
     pub ltp: OrderPrice
+}
+
+impl<'a> Book<'a> {
+    pub fn new(id: BookId, name: String, ticker: String) -> Self {
+        Book {
+            id,
+            name,
+            ticker,
+            bids: Side::new(),
+            asks: Side::new(),
+            ltp: 0
+        }
+    }
 }
 
